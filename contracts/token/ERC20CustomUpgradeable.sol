b886a3dfd2b86d72fbba3d585e22e19c40566194
YosephKS <yosephkurniasoenggoro@gmail.com>
2022-02-04 23:00:52 +0700

    add airdrop within the ERC20 smart contract

--- contracts/token/ERC20CustomUpgradeable.sol
+++ contracts/token/ERC20CustomUpgradeable.sol
@@ -11,12 +11,25 @@ import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpg
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20FlashMintUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
-contract MyToken is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, PausableUpgradeable, AccessControlUpgradeable, ERC20PermitUpgradeable, ERC20VotesUpgradeable, ERC20FlashMintUpgradeable, UUPSUpgradeable {
+
+/**
+ * @notice This is the contract for ALPS token
+ */
+contract ERC20CustomUpgradeable is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, PausableUpgradeable, AccessControlUpgradeable, ERC20PermitUpgradeable, ERC20VotesUpgradeable, ERC20FlashMintUpgradeable, UUPSUpgradeable {
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
     bytes32 public  merkleRoot;
+    mapping(address => bool) public airdropClaimed;
+
+    /*
+     * @notice Emitted after a successful token claim
+     * @param to recipient of claim
+     * @param amount of tokens claimed
+     */
+    event Claim(address indexed to, uint256 amount);
 
     function initialize(string memory _name, string memory _symbol, bytes32  _merkleRoot) initializer public {
         __ERC20_init(_name, _symbol);
@@ -46,6 +59,36 @@ contract MyToken is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, P
     function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
     }
+
+    /**
+     * @notice Allows claiming tokens if address is part of merkle tree
+     * @param to address of claimee
+     * @param amount of tokens owed to claimee
+     * @param proof merkle proof to prove address and amount are in tree
+     */
+   
+  function claim(
+      address to,
+      uint256 amount,
+      bytes32[] calldata proof
+    ) whenNotPaused external {
+    // Throw if address has already claimed tokens
+    require(!airdropClaimed[to], "ERC20: Airdrop has been claimed!");
+
+    // Verify merkle proof, or revert if not in tree
+    bytes32 leaf = keccak256(abi.encodePacked(to, amount));
+    bool isValidLeaf = MerkleProof.verify(proof, merkleRoot, leaf);
+    require(!isValidLeaf, "ERC20: Address has no Airdrop claim!");
+
+    // Set address to claimed
+    airdropClaimed[to] = true;
+
+    // Mint tokens to address
+    _mint(to, amount);
+
+    // Emit claim event
+    emit Claim(to, amount);
+  }
 
     function _beforeTokenTransfer(address from, address to, uint256 amount)
         internal
