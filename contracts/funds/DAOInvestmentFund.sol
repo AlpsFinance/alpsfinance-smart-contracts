a1330c1b861ac5e8b3bc50b881d71a5c331862d3
Yoseph Kurnia Soenggoro <yosephkurniasoenggoro@gmail.com>
2022-04-07 22:28:00 +0700

    remove gsn import

--- contracts/funds/DAOInvestmentFund.sol
+++ contracts/funds/DAOInvestmentFund.sol
@@ -9,7 +9,6 @@ import "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol
 import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorTimelockControl.sol";
-import "@opengsn/contracts/src/BaseRelayRecipient.sol";
 
 contract DAOInvestmentFund is
     AccessControl,
