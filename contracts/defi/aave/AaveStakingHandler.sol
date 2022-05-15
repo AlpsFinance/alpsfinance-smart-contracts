22b12f1137779f348bdd552f006cc06bd5acf4c6
Yoseph Kurnia Soenggoro <yosephkurniasoenggoro@gmail.com>
2022-05-15 17:54:44 +0700

    complete WETH handler contracts

--- contracts/defi/aave/AaveStakingHandler.sol
+++ contracts/defi/aave/AaveStakingHandler.sol
@@ -2,129 +2,125 @@
 
 pragma solidity ^0.8.11;
 
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {IERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/draft-IERC20Permit.sol";
-import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import {IStakedAave} from "./interfaces/IStakedAave.sol";
+import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import {IERC20Permit} from '@openzeppelin/contracts/token/ERC20/extensions/draft-IERC20Permit.sol';
+import {SafeMath} from '@openzeppelin/contracts/utils/math/SafeMath.sol';
+import {IStakedAave} from './interfaces/IStakedAave.sol';
 
 contract AaveStakingHandler {
-    using SafeMath for uint256;
+  using SafeMath for uint256;
 
-    address public AAVE_TOKEN;
-    address public STK_AAVE_TOKEN;
-    address public MULTISIG_WALLET;
+  address public AAVE_TOKEN;
+  address public STK_AAVE_TOKEN;
+  address public MULTISIG_WALLET;
 
-    // ============== EVENT ==============
-    event StakeAave(address staker, uint256 stakeAmount, uint256 fee);
-    event ClaimAave(
-        address staker,
-        uint256 claimAmount,
-        uint256 claimTimestamp
+  // ============== EVENT ==============
+  event StakeAave(address staker, uint256 stakeAmount, uint256 fee);
+  event ClaimAave(address staker, uint256 claimAmount, uint256 claimTimestamp);
+
+  // ============== ERROR ==============
+  error InvalidZeroAmount();
+
+  // ============== MODIFIER ==============
+  modifier onlyValidAmount(uint256 amount) {
+    if (amount <= 0) revert InvalidZeroAmount();
+    _;
+  }
+
+  constructor(
+    address aaveTokenAddress,
+    address stkAaveTokenAddress,
+    address multisigAddress
+  ) {
+    AAVE_TOKEN = aaveTokenAddress;
+    STK_AAVE_TOKEN = stkAaveTokenAddress;
+    MULTISIG_WALLET = multisigAddress;
+  }
+
+  /**
+   * @dev Stake Aave and minting StkAave
+   * @param amount - (uint256)
+   */
+  function stake(uint256 amount) external onlyValidAmount(amount) {
+    _stake(amount);
+  }
+
+  /**
+   * @dev Stake Aave and minting StkAave with Permit (no approval)
+   * @param amount - (uint256)
+   */
+  function stakeWithPermit(
+    uint256 amount,
+    uint8 v,
+    bytes32 r,
+    bytes32 s
+  ) external onlyValidAmount(amount) {
+    IERC20Permit(AAVE_TOKEN).permit(
+      msg.sender,
+      address(this),
+      amount,
+      block.timestamp + 15 minutes,
+      v,
+      r,
+      s
     );
+    _stake(amount);
+  }
 
-    // ============== ERROR ==============
-    error InvalidZeroAmount();
+  /**
+   * @dev Unstake Aave by burning StkAave
+   * @param amount - (uint256) Amount of StkAave that would like to be unstaked
+   */
+  function unstake(uint256 amount) external onlyValidAmount(amount) {
+    _unstake(amount);
+  }
 
-    // ============== MODIFIER ==============
-    modifier onlyValidAmount(uint256 amount) {
-        if (amount <= 0) revert InvalidZeroAmount();
-        _;
-    }
+  /**
+   * @dev Unstake Aave by burning StkAave with Permit (no approval)
+   * @param amount - (uint256)
+   */
+  function unstakeWithPermit(
+    uint256 amount,
+    uint8 v,
+    bytes32 r,
+    bytes32 s
+  ) external onlyValidAmount(amount) {
+    IERC20Permit(STK_AAVE_TOKEN).permit(
+      msg.sender,
+      address(this),
+      amount,
+      block.timestamp + 15 minutes,
+      v,
+      r,
+      s
+    );
+    _unstake(amount);
+  }
 
-    constructor(
-        address aaveTokenAddress,
-        address stkAaveTokenAddress,
-        address multisigAddress
-    ) {
-        AAVE_TOKEN = aaveTokenAddress;
-        STK_AAVE_TOKEN = stkAaveTokenAddress;
-        MULTISIG_WALLET = multisigAddress;
-    }
+  // ==================== INTERNAL FUNCTIONS ====================
 
-    /**
-     * @dev Stake Aave and minting StkAave
-     * @param amount - (uint256)
-     */
-    function stake(uint256 amount) external onlyValidAmount(amount) {
-        _stake(amount);
-    }
+  function _stake(uint256 amount) internal {
+    IERC20(AAVE_TOKEN).transferFrom(msg.sender, address(this), amount);
 
-    /**
-     * @dev Stake Aave and minting StkAave with Permit (no approval)
-     * @param amount - (uint256)
-     */
-    function stakeWithPermit(
-        uint256 amount,
-        uint8 v,
-        bytes32 r,
-        bytes32 s
-    ) external onlyValidAmount(amount) {
-        IERC20Permit(AAVE_TOKEN).permit(
-            msg.sender,
-            address(this),
-            amount,
-            block.timestamp + 15 minutes,
-            v,
-            r,
-            s
-        );
-        _stake(amount);
-    }
+    // Stake AAVE
+    IERC20(AAVE_TOKEN).approve(STK_AAVE_TOKEN, amount);
+    uint256 stakingFee = SafeMath.div(amount, 1000); // 0.1% fee
+    IStakedAave(STK_AAVE_TOKEN).stake(
+      msg.sender,
+      SafeMath.sub(amount, stakingFee)
+    );
+    IStakedAave(STK_AAVE_TOKEN).stake(MULTISIG_WALLET, stakingFee);
 
-    /**
-     * @dev Unstake Aave by burning StkAave
-     * @param amount - (uint256) Amount of StkAave that would like to be unstaked
-     */
-    function unstake(uint256 amount) external onlyValidAmount(amount) {
-        _unstake(amount);
-    }
+    emit StakeAave(msg.sender, amount, stakingFee);
+  }
 
-    /**
-     * @dev Unstake Aave by burning StkAave with Permit (no approval)
-     * @param amount - (uint256)
-     */
-    function unstakeWithPermit(
-        uint256 amount,
-        uint8 v,
-        bytes32 r,
-        bytes32 s
-    ) external onlyValidAmount(amount) {
-        IERC20Permit(STK_AAVE_TOKEN).permit(
-            msg.sender,
-            address(this),
-            amount,
-            block.timestamp + 15 minutes,
-            v,
-            r,
-            s
-        );
-        _unstake(amount);
-    }
-
-    // ==================== INTERNAL FUNCTIONS ====================
-
-    function _stake(uint256 amount) internal {
-        IERC20(AAVE_TOKEN).transferFrom(msg.sender, address(this), amount);
-
-        // Stake AAVE
-        IERC20(AAVE_TOKEN).approve(STK_AAVE_TOKEN, amount);
-        uint256 stakingFee = SafeMath.div(amount, 1000); // 0.1% fee
-        IStakedAave(STK_AAVE_TOKEN).stake(
-            msg.sender,
-            SafeMath.sub(amount, stakingFee)
-        );
-        IStakedAave(STK_AAVE_TOKEN).stake(MULTISIG_WALLET, stakingFee);
-
-        emit StakeAave(msg.sender, amount, stakingFee);
-    }
-
-    function _unstake(uint256 amount) internal {
-        IERC20(STK_AAVE_TOKEN).transferFrom(msg.sender, address(this), amount);
-        uint256 unstakingFee = SafeMath.div(amount, 1000); // 0.1% fee
-        IStakedAave(STK_AAVE_TOKEN).redeem(MULTISIG_WALLET, unstakingFee);
-        IStakedAave(STK_AAVE_TOKEN).redeem(
-            msg.sender,
-            SafeMath.sub(amount, unstakingFee)
-        );
-    }
+  function _unstake(uint256 amount) internal {
+    IERC20(STK_AAVE_TOKEN).transferFrom(msg.sender, address(this), amount);
+    uint256 unstakingFee = SafeMath.div(amount, 1000); // 0.1% fee
+    IStakedAave(STK_AAVE_TOKEN).redeem(MULTISIG_WALLET, unstakingFee);
+    IStakedAave(STK_AAVE_TOKEN).redeem(
+      msg.sender,
+      SafeMath.sub(amount, unstakingFee)
+    );
+  }
 }
--- contracts/defi/weth/WETHHandler.sol
+++ contracts/defi/weth/WETHHandler.sol
@@ -9,19 +9,50 @@ import {IWETH} from './interfaces/IWETH.sol';
 contract WETHHandler is ReentrancyGuard {
   using SafeMath for uint256;
 
-  IWETH public WETH;
+  IWETH public immutable WETH;
 
   constructor(address wethAddress) {
     WETH = IWETH(wethAddress);
   }
 
+  // ============== EVENT ==============
+  event DepositWrapped(address user, uint256 depositAmount, uint256 depositFee);
+  event WithdrawWrapped(
+    address user,
+    uint256 withdrawalAmount,
+    uint256 withdrawalFee
+  );
+
+  // ============== ERROR ==============
+  error InvalidZeroAmount();
+
+  /**
+   * @dev Deposit ETH for WETH (or native for Wrapped Native token in general)
+   */
   function deposit() external payable nonReentrant {
-    uint256 fee = SafeMath.div(msg.value, 1000); // 0.1% fee
+    if (msg.value <= 0) revert InvalidZeroAmount();
+    uint256 depositFee = SafeMath.div(msg.value, 1000); // 0.1% fee
     // SEND Fee to MULTISIG
-    uint256 depositValue = SafeMath.sub(msg.value, fee);
+    uint256 depositValue = SafeMath.sub(msg.value, depositFee);
     WETH.deposit{value: depositValue}();
     WETH.transfer(msg.sender, depositValue);
+
+    emit DepositWrapped(msg.sender, depositValue, depositFee);
   }
 
-  function withdraw() external nonReentrant {}
+  /**
+   * @dev Withdraw WETH for ETH (or Wrapped Native token for Native token in general)
+   * @param amount (uint256) - amount of WETH to be burnt and withdraw ETH
+   */
+  function withdraw(uint256 amount) external nonReentrant {
+    if (amount <= 0) revert InvalidZeroAmount();
+    WETH.transferFrom(msg.sender, address(this), amount);
+    WETH.withdraw(amount);
+    uint256 withdrawalFee = SafeMath.div(amount, 1000); // 0.1% fee
+    uint256 withdrawalValue = SafeMath.sub(amount, withdrawalFee);
+    // SEND Fee to MULTISIG
+    payable(msg.sender).transfer(withdrawalValue);
+
+    emit WithdrawWrapped(msg.sender, withdrawalValue, withdrawalFee);
+  }
 }
