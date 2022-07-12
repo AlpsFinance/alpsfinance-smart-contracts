1fb3a70b9141770213119dbba30fc54ee27b9657
Yoseph Kurnia Soenggoro <yosephkurniasoenggoro@gmail.com>
2022-07-12 21:40:32 +0700

    building StrategyManager.sol initial

---
+++ contracts/defi/StrategyManager.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: AGPL-3.0
+
+pragma solidity ^0.8.11;
+
+import {Ownable} from '@openzeppelin/contracts/access/Ownable.sol';
+
+contract StrategyManager is Ownable {
+  function setAdmin() public onlyOwner {}
+
+  function setStrategyFee() public onlyOwner {}
+
+  function registerStrategy(bytes32 strategyId, uint256 fee) public {}
+
+  function executeStrategy() public {}
+}
