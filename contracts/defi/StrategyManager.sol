0428766bb06814a5718f34e02aeac41d31aa11a8
Yoseph Kurnia Soenggoro <yosephkurniasoenggoro@gmail.com>
2022-07-19 13:52:11 +0700

    minor update on StrategyManager

--- contracts/defi/StrategyManager.sol
+++ contracts/defi/StrategyManager.sol
@@ -6,8 +6,6 @@ import {Ownable} from '@openzeppelin/contracts/access/Ownable.sol';
 import {FeeManager} from '../libraries/FeeManager.sol';
 
 contract StrategyManager is Ownable {
-  function setAdmin() public onlyOwner {}
-
   function setStrategyFee() public onlyOwner {}
 
   function registerStrategy(bytes32 strategyId, uint256 fee) public {}
